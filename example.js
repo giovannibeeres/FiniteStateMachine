46348cefcccde34e3d61ee74d052e42865d318f6
giovannibeeres <giomaubeeres@googlemail.com>
2015-10-29 23:10:53 +0100

    example added

---
+++ example.js
@@ -0,0 +1,14 @@
+(function(){
+	
+	const
+	START_STATE = "START_STATE",
+	TARGET_STATE = "TARGET_STATE",
+	
+	StartState = new State(),
+	TargetState = new State();
+	
+	/* 
+	* working..
+	*/
+	
+})()
