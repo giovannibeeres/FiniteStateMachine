8070fbfebe245cb797f993461ec63d082ce67f7d
giovannibeeres <giomaubeeres@googlemail.com>
2015-10-29 23:14:11 +0100

    machine example script

--- example.js
+++ example.js
@@ -5,7 +5,13 @@
 	TARGET_STATE = "TARGET_STATE",
 	
 	StartState = new State(),
-	TargetState = new State();
+	TargetState = new State(),
+	Machine = new StateMachine();
+	
+	Machine.addState(START_STATE,StartState,[TARGET_STATE]);
+	Machine.addState(TARGET_STATE,TargetState,[START_STATE]);
+	
+	Machine.setState(START_STATE);
 	
 	/* 
 	* working..
